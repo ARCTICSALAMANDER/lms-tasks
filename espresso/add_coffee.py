1a79929a86c4cffbf4502ec5f5bd4c935c37807f
ARCTIC_SALAMANDER <ruslanau365@gmail.com>
2026-03-06 19:55:52 +0300

    connected the .ui files, sorted everything into folders, added the .exe file

--- espresso/add_coffee.py
+++ espresso/add_coffee.py
@@ -1,99 +1,93 @@
-from PyQt6.QtWidgets import (QApplication, QWidget, QHBoxLayout, QVBoxLayout,
-                             QLabel, QTextEdit, QLineEdit, QPushButton)
-import sys
+from PyQt6 import QtCore, QtGui, QtWidgets
 import sqlite3
+import sys
 
 
-class AddCoffee(QWidget):
-    def __init__(self, main_window: QWidget):
+class Ui_Form(object):
+    def setupUi(self, Form):
+        Form.setObjectName("Form")
+        Form.resize(400, 300)
+        self.label = QtWidgets.QLabel(parent=Form)
+        self.label.setGeometry(QtCore.QRect(30, 10, 210, 21))
+        self.label.setObjectName("label")
+        self.label_2 = QtWidgets.QLabel(parent=Form)
+        self.label_2.setGeometry(QtCore.QRect(30, 40, 47, 13))
+        self.label_2.setObjectName("label_2")
+        self.name_text = QtWidgets.QLineEdit(parent=Form)
+        self.name_text.setGeometry(QtCore.QRect(30, 60, 113, 20))
+        self.name_text.setObjectName("name_text")
+        self.label_3 = QtWidgets.QLabel(parent=Form)
+        self.label_3.setGeometry(QtCore.QRect(30, 90, 51, 16))
+        self.label_3.setObjectName("label_3")
+        self.roast_text = QtWidgets.QLineEdit(parent=Form)
+        self.roast_text.setGeometry(QtCore.QRect(30, 110, 113, 20))
+        self.roast_text.setObjectName("roast_text")
+        self.label_4 = QtWidgets.QLabel(parent=Form)
+        self.label_4.setGeometry(QtCore.QRect(30, 140, 141, 16))
+        self.label_4.setObjectName("label_4")
+        self.grind_text = QtWidgets.QLineEdit(parent=Form)
+        self.grind_text.setGeometry(QtCore.QRect(30, 160, 113, 20))
+        self.grind_text.setObjectName("grind_text")
+        self.label_5 = QtWidgets.QLabel(parent=Form)
+        self.label_5.setGeometry(QtCore.QRect(200, 40, 47, 13))
+        self.label_5.setObjectName("label_5")
+        self.taste_text = QtWidgets.QTextEdit(parent=Form)
+        self.taste_text.setGeometry(QtCore.QRect(200, 60, 191, 121))
+        self.taste_text.setObjectName("taste_text")
+        self.label_6 = QtWidgets.QLabel(parent=Form)
+        self.label_6.setGeometry(QtCore.QRect(30, 190, 81, 16))
+        self.label_6.setObjectName("label_6")
+        self.package_text = QtWidgets.QLineEdit(parent=Form)
+        self.package_text.setGeometry(QtCore.QRect(30, 210, 113, 20))
+        self.package_text.setObjectName("package_text")
+        self.label_7 = QtWidgets.QLabel(parent=Form)
+        self.label_7.setGeometry(QtCore.QRect(30, 240, 91, 16))
+        self.label_7.setObjectName("label_7")
+        self.price_text = QtWidgets.QLineEdit(parent=Form)
+        self.price_text.setGeometry(QtCore.QRect(30, 260, 113, 20))
+        self.price_text.setObjectName("price_text")
+        self.add_btn = QtWidgets.QPushButton(parent=Form)
+        self.add_btn.setGeometry(QtCore.QRect(200, 190, 75, 23))
+        self.add_btn.setObjectName("add_btn")
+        self.back_btn = QtWidgets.QPushButton(parent=Form)
+        self.back_btn.setGeometry(QtCore.QRect(320, 10, 75, 23))
+        self.back_btn.setObjectName("back_btn")
+        
+        self.err_label = QtWidgets.QLabel(parent=Form)
+        self.err_label.setGeometry(QtCore.QRect(200, 220, 180, 20))
+        self.err_label.setStyleSheet("color: red;")
+        self.err_label.setText("")
+
+        self.retranslateUi(Form)
+        QtCore.QMetaObject.connectSlotsByName(Form)
+
+    def retranslateUi(self, Form):
+        _translate = QtCore.QCoreApplication.translate
+        Form.setWindowTitle(_translate("Form", "Добавить/Изменить кофе"))
+        self.label.setText(_translate("Form", "Добавить информацию о сорте кофе"))
+        self.label_2.setText(_translate("Form", "название"))
+        self.label_3.setText(_translate("Form", "прожарка"))
+        self.label_4.setText(_translate("Form", "тип (молотый/в зернах)"))
+        self.label_5.setText(_translate("Form", "вкус"))
+        self.label_6.setText(_translate("Form", "объем пачки"))
+        self.label_7.setText(_translate("Form", "цена за пачку"))
+        self.add_btn.setText(_translate("Form", "Добавить"))
+        self.back_btn.setText(_translate("Form", "Назад"))
+
+
+class AddCoffee(QtWidgets.QWidget, Ui_Form):
+    def __init__(self, main_window):
         super().__init__()
-        self.setGeometry(300, 300, 650, 450)
-        self.setWindowTitle("Эспрессо")
-
+        self.setupUi(self)
+        
         self.main_window = main_window
         self.coffee_data = dict()
 
         self.con = sqlite3.connect("coffee.sqlite")
         self.cur = self.con.cursor()
 
-        self.setup_ui()
-
-    def setup_ui(self):
-        self.full_layout = QVBoxLayout(self)
-
-        self.header_layout = QHBoxLayout()
-
-        self.title_label = QLabel("Добавить запись о кофе")
-        self.title_label.setStyleSheet("font-size: 18px; font-weight: bold;")
-
-        self.back_btn = QPushButton("Назад")
-        self.back_btn.setFixedWidth(80)
-        self.back_btn.pressed.connect(self.go_back)
-
-        self.header_layout.addWidget(self.title_label)
-        self.header_layout.addStretch()
-        self.header_layout.addWidget(self.back_btn)
-
-        self.full_layout.addLayout(self.header_layout)
-
-        self.content_layout = QHBoxLayout()
-
-        self.left_layout = QVBoxLayout()
-
-        self.name_label = QLabel("Название")
-        self.name_text = QLineEdit()
-
-        self.roast_label = QLabel("Обжарка")
-        self.roast_text = QLineEdit()
-
-        self.type_label = QLabel("Тип (молотый/в зернах)")
-        self.type_text = QLineEdit()
-
-        self.price_label = QLabel("Цена за упаковку")
-        self.price_text = QLineEdit()
-
-        self.volume_label = QLabel("Объем упаковки")
-        self.volume_text = QLineEdit()
-
-        widgets = [
-            self.name_label, self.name_text,
-            self.roast_label, self.roast_text,
-            self.type_label, self.type_text,
-            self.price_label, self.price_text,
-            self.volume_label, self.volume_text
-        ]
-
-        for w in widgets:
-            if isinstance(w, QLineEdit):
-                w.setFixedHeight(30)
-            self.left_layout.addWidget(w)
-
-        self.left_layout.addStretch()
-
-        self.right_layout = QVBoxLayout()
-
-        self.taste_label = QLabel("Вкус")
-        self.taste_text = QTextEdit()
-        self.taste_text.setMinimumWidth(250)
-
-        self.add_btn = QPushButton("Добавить")
-        self.add_btn.setFixedHeight(40)
-        self.add_btn.setStyleSheet(
-            "font-weight: bold; background-color: #f0f0f0;")
-        self.add_btn.pressed.connect(self.add_coffee)
-
-        self.err_label = QLabel("")
-        self.err_label.setStyleSheet("color: red;")
-
-        self.right_layout.addWidget(self.taste_label)
-        self.right_layout.addWidget(self.taste_text)
-        self.right_layout.addWidget(self.add_btn)
-        self.right_layout.addWidget(self.err_label)
-
-        self.content_layout.addLayout(self.left_layout, stretch=1)
-        self.content_layout.addLayout(self.right_layout, stretch=1)
-
-        self.full_layout.addLayout(self.content_layout)
+        self.back_btn.clicked.connect(self.go_back)
+        self.add_btn.clicked.connect(self.add_coffee)
 
     def go_back(self):
         self.close()
@@ -102,19 +96,18 @@ class AddCoffee(QWidget):
     def add_coffee(self):
         name = self.name_text.text()
         roast = self.roast_text.text()
-        coffee_type = self.type_text.text()
+        coffee_type = self.grind_text.text()
+        taste = self.taste_text.toPlainText()
 
         try:
             price = int(self.price_text.text())
-            volume = int(self.volume_text.text())
+            volume = int(self.package_text.text())
         except ValueError:
-            self.err_label.setText("не все поля заполнены")
+            self.err_label.setText("Цена и объем — числа!")
             return
 
-        taste = self.taste_text.toPlainText()
-
         if not name or not roast or not coffee_type or not taste:
-            self.err_label.setText("не все поля заполнены")
+            self.err_label.setText("Не все поля заполнены")
             return
 
         self.coffee_data = {
@@ -127,8 +120,8 @@ class AddCoffee(QWidget):
         }
 
         self.add_coffee_to_db()
-
-        self.err_label.setText("Добавлено!")
+        self.err_label.setStyleSheet("color: green;")
+        self.err_label.setText("Успешно добавлено!")
 
     def add_coffee_to_db(self):
         if self.coffee_data:
@@ -141,5 +134,4 @@ class AddCoffee(QWidget):
                   self.coffee_data['taste'], 
                   self.coffee_data['price'], 
                   self.coffee_data['volume']))
-            
             self.con.commit()
--- espresso/main.py
+++ espresso/main.py
@@ -1,71 +1,85 @@
+from PyQt6 import QtCore, QtWidgets
 from PyQt6.QtSql import QSqlDatabase, QSqlTableModel
-from PyQt6.QtWidgets import QWidget, QLabel, QTableView, QVBoxLayout, QApplication, QPushButton
+from PyQt6.QtWidgets import QApplication
 import sys
 import sqlite3
 from add_coffee import AddCoffee
 
 
-class Espresso(QWidget):
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(800, 600)
+        self.centralwidget = QtWidgets.QWidget(parent=MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.label = QtWidgets.QLabel(parent=self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(10, 10, 241, 31))
+        self.label.setObjectName("label")
+        self.tableView = QtWidgets.QTableView(parent=self.centralwidget)
+        self.tableView.setGeometry(QtCore.QRect(10, 50, 781, 461))
+        self.tableView.setObjectName("tableView")
+        
+        self.add_btn = QtWidgets.QPushButton("Добавить", parent=self.centralwidget)
+        self.add_btn.setGeometry(QtCore.QRect(10, 520, 100, 30))
+        
+        self.reload_btn = QtWidgets.QPushButton("Обновить", parent=self.centralwidget)
+        self.reload_btn.setGeometry(QtCore.QRect(120, 520, 100, 30))
+
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(parent=MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 21))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(parent=MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "Эспрессо - Главная"))
+        self.label.setText(_translate("MainWindow", "Информация о кофе"))
+
+
+class Espresso(QtWidgets.QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
-        self.setGeometry(300, 300, 800, 600)
-        self.setWindowTitle("Эспрессо")
+        self.setupUi(self)
 
         self.db_setup()
-        self.setupUi()
+        self.init_db_model()
 
-        self.add_coffee = AddCoffee(self)
+        self.add_coffee_window = AddCoffee(self)
 
-    def setupUi(self):
-        self.mainLayout = QVBoxLayout(self)
+        self.add_btn.clicked.connect(self.add_coffee_pressed)
+        self.reload_btn.clicked.connect(self.reload_table)
 
-        self.label = QLabel("Кофе info")
-        self.label.setStyleSheet('''
-            font-size: 25px;
-            font-family: sans-serif;
-        ''')
-        self.mainLayout.addWidget(self.label)
-
-        self.reload_btn = QPushButton("перезагрузить таблицу")
-        self.reload_btn.setFixedWidth(150)
-        self.reload_btn.pressed.connect(self.reload_table)
-        self.mainLayout.addWidget(self.reload_btn)
-
+    def init_db_model(self):
         self.db = QSqlDatabase.addDatabase('QSQLITE')
         self.db.setDatabaseName('coffee.sqlite')
-        self.db.open()
+        if not self.db.open():
+            print("Не удалось открыть базу данных")
+            return
 
-        model = QSqlTableModel(self, self.db)
-        model.setTable('coffee_info')
-        model.select()
-
-        self.table = QTableView()
-        self.table.resize(550, 800)
-        self.table.setModel(model)
-        self.mainLayout.addWidget(self.table)
-
-        self.add_btn = QPushButton("Добавить")
-        self.add_btn.pressed.connect(self.add_coffee_pressed)
-        self.mainLayout.addWidget(self.add_btn)
-
-        self.setLayout(self.mainLayout)
+        self.model = QSqlTableModel(self, self.db)
+        self.model.setTable('coffee_info')
+        self.model.select()
+        
+        self.tableView.setModel(self.model)
 
     def reload_table(self):
-        model = QSqlTableModel(self, self.db)
-        model.setTable('coffee_info')
-        model.select()
-
-        self.table.setModel(model)
+        self.model.select()
 
     def add_coffee_pressed(self):
         self.hide()
-        self.add_coffee.show()
+        self.add_coffee_window.show()
 
     def db_setup(self):
-        self.con = sqlite3.connect("coffee.sqlite")
-        self.cur = self.con.cursor()
-
-        self.cur.execute('''
+        con = sqlite3.connect("coffee.sqlite")
+        cur = con.cursor()
+        cur.execute('''
             CREATE TABLE IF NOT EXISTS coffee_info (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
                 name TEXT NOT NULL,
@@ -77,28 +91,17 @@ class Espresso(QWidget):
             )
         ''')
 
-        data = self.cur.execute('''
-            SELECT * FROM coffee_info
-        ''').fetchall()
-
+        data = cur.execute('SELECT * FROM coffee_info').fetchone()
         if not data:
-            self.cur.execute('''
+            cur.execute('''
                 INSERT INTO coffee_info (name, roast, grind, taste, price, package)
-                VALUES ("Арабика", "Средняя", "В зернах", "Вкусный", 400, 300)
+                VALUES 
+                ("Арабика", "Средняя", "В зернах", "Вкусный", 400, 300),
+                ("Робуста", "Темная", "Молотая", "Не очень вкусная", 700, 300),
+                ("Арабика", "Венская", "В зернах", "яблоко, лесной орех", 250, 50)
             ''')
-
-            self.cur.execute('''
-                INSERT INTO coffee_info (name, roast, grind, taste, price, package)
-                VALUES ("Робуста", "Темная", "Молотая", "Не очень вкусная", 700, 300)
-            ''')
-
-            self.cur.execute('''
-                INSERT INTO coffee_info (name, roast, grind, taste, price, package)
-                VALUES ("Арабика", "Венская", "В зернах", "яблоко, лесной орех", 250, 50)
-            ''')
-
-            self.con.commit()
-        self.con.close()
+            con.commit()
+        con.close()
 
 
 if __name__ == '__main__':
@@ -106,4 +109,3 @@ if __name__ == '__main__':
     ex = Espresso()
     ex.show()
     sys.exit(app.exec())
-        
