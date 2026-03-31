1acfacf90b2b46ece174a3b9b9c5d6168541be7d
ARCTIC_SALAMANDER <ruslanau365@gmail.com>
2026-03-31 19:41:11 +0300

    a lot of code

---
+++ users_n_jobs/data/jobs.py
@@ -0,0 +1,23 @@
+import sqlalchemy
+from sqlalchemy import orm
+import datetime
+from .db_session import SqlAlchemyBase
+
+
+class Jobs(SqlAlchemyBase):
+    __tablename__ = 'jobs'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer,
+                           primary_key=True, autoincrement=True)
+    # sqlalchemy.Column(sqlalchemy.Integer, nullable=True)
+    team_leader = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey("users.id"))
+    job = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    work_size = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)
+    collaborators = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    start_time = sqlalchemy.Column(
+        sqlalchemy.DateTime, default=datetime.datetime.now)
+    end_time = sqlalchemy.Column(
+        sqlalchemy.DateTime, default=datetime.datetime.now)
+    is_finished = sqlalchemy.Column(sqlalchemy.Boolean)
+
+    user = orm.relationship('User')
